02d117869e4db2a719c4790da1bbfda957f6ab7a
Vittal Katwe <109062125+vittalkatwe@users.noreply.github.com>
2025-01-14 22:49:46 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Vite's default port
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
